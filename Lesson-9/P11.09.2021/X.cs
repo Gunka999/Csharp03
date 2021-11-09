d75e38ca0ecdd20a9974e962dd410d84be5b4913
Mina <41920626+Minayaff@users.noreply.github.com>
2021-11-09 10:42:01 +0400

    Add files via upload

---
+++ Lesson-9/P11.09.2021/X.cs
@@ -0,0 +1,13 @@
+﻿using Solution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P18._10._2021
+{
+    class X
+    {
+    }
+}
