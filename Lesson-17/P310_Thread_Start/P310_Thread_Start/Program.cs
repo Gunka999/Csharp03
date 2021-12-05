84661d68e2990461829aeecac730ee53be721530
Mina <41920626+Minayaff@users.noreply.github.com>
2021-12-05 23:07:42 +0400

    Add files via upload

---
+++ Lesson-17/P310_Thread_Start/P310_Thread_Start/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace P310_Thread_Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread threadLoopA = new Thread(LoopA);
+            Thread threadLoopB = new Thread(LoopB);
+
+            //threadLoopA.IsBackground = true;
+            //threadLoopB.IsBackground = true;
+
+            threadLoopA.Start();
+            threadLoopB.Start();
+
+            //Thread.Sleep(1000);
+        }
+
+        static void LoopA()
+        {
+            Thread.Sleep(1000);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Loop A: {i}");
+            }
+
+        }
+
+        static void LoopB()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Loop B: {i}");
+            }
+        }
+    }
+}
---
+++ Lesson-17/P310_Thread_Start/P310_Thread_WinForm/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P310_Thread_WinForm
+{
+    public partial class Form1 : Form
+    {
+        private long? Total = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(Sum);
+            thread.Start(100000000);
+
+            while(Total == null)
+            {
+                label1.Text = "Waiting new Thread to finish calculation";
+            }
+
+            textBox1.Text = Total.ToString();
+        }
+
+        private void Sum(object number)
+        {
+            long sum = 0;
+            for (int i = 0; i < Convert.ToInt64(number); i++)
+            {
+                sum += i;
+            }
+
+            Total = sum;
+        }
+    }
+}
